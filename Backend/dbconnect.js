513a0c09ce91c7b136984c48e0e7262a25c8d2a6
Prince-Deka <princedeka00@gmail.com>
2023-01-12 23:50:34 +0530

    first commit

--- Backend/dbconnect.js
+++ Backend/dbconnect.js
@@ -3,7 +3,7 @@ const url = 'mongodb://localhost:27017/Record';
 mongoose.connect(url).then(()=>{
   console.log('Node-Js to MongoDb connection Established');
 }).catch((err)=>{
-  console.log('Error in connect the server'+json.stringify(err,undefined,2));
-  prcess.exit();
+  console.log('Error in connect the server'+JSON.stringify(err,undefined,2));
+  process.exit();
 })
 module.exports = mongoose;
